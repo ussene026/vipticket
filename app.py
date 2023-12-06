6197a861623a12d2fac8eb06ed8e267818aed2a5
Santos Ussene <ussene026@outlook.com>
2023-12-07 01:22:40 +0200

    ADD: Complete login process

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, login_user, UserMixin, login_required, logout_user, current_user
 import mysql.connector
 
 app = Flask(__name__)
 app.secret_key = 'djfljdfljfnkjsfhjfshjkfjfjfhjdhfdjhdfu'
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 db = mysql.connector.connect(
     host="localhost",
@@ -14,18 +16,62 @@ db = mysql.connector.connect(
 )
 cursor = db.cursor()
 
+class User(UserMixin):
+    pass
+
 def telefone_existente(telefone):
     query = "SELECT COUNT(*) FROM users WHERE Telefone = %s"
     cursor.execute(query, (telefone,))
     result = cursor.fetchone()
     return result[0] > 0
 
+@login_manager.user_loader
+def load_user(user_id):
+    # Função para carregar usuário pelo ID
+    query = "SELECT * FROM users WHERE ID = %s"
+    cursor.execute(query, (user_id,))
+    user_data = cursor.fetchone()
+    if user_data:
+        user = User()
+        user.id = user_data[0]
+        user.nome = user_data[1]
+        user.telefone = user_data[2]
+        user.password = user_data[3]
+        return user
+    return None
+
+@login_manager.unauthorized_handler
+def unauthorized_callback():
+    flash("danger", "Ocorreu um erro. Faça login para continuar.")
+    return redirect(url_for("login"))
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        telefone = request.form["telefone"]
+        password = request.form["password"]
+
+        query = "SELECT * FROM users WHERE Telefone = %s"
+        cursor.execute(query, (telefone,))
+        user_data = cursor.fetchone()
+
+        if user_data and bcrypt.check_password_hash(user_data[3], password):
+            user = User()
+            user.id = user_data[0]
+            user.nome = user_data[1]
+            user.telefone = user_data[2]
+            user.password = user_data[3]
+
+            login_user(user)
+            flash("success", "Autenticado com sucesso!")
+            return redirect(url_for("dashboard"))
+        else:
+            flash("danger", "Credenciais inválidas. Tente novamente.")
+
     return render_template("login.html")
 
 @app.route("/register", methods=["GET", "POST"])
@@ -52,8 +98,16 @@ def register():
     return render_template("register.html")
 
 @app.route("/dashboard")
+@login_required
 def dashboard():
     return render_template("dashboard.html")
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("success", "Sessão terminada!")
+    return redirect(url_for("index"))
 
 if __name__ == '__main__':
     app.run(debug=True)
