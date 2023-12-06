c10ec019662ff54e880e20cd9e8abe81d63d8ae7
Santos Ussene <ussene026@outlook.com>
2023-12-06 23:07:30 +0200

    REFACTOR: Code base

--- app.py
+++ app.py
@@ -2,12 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
 
-# app.run(debug=True)
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
